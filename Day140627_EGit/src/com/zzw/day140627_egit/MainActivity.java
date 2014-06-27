f4493cd1cbbfcb6fd1b91c05a33c00c0ca111d01
mutougege <joyzzw@yeah.net>
2014-06-27 15:57:06 +0800

    333333333333333333

--- Day140627_EGit/src/com/zzw/day140627_egit/MainActivity.java
+++ Day140627_EGit/src/com/zzw/day140627_egit/MainActivity.java
@@ -3,13 +3,19 @@ package com.zzw.day140627_egit;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	TextView tv;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.activity_main);
+		
+		tv = (TextView) findViewById(R.id.tv);
 	}
 
 
